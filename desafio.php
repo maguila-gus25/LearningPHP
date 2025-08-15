a61818d72b0c8a6c1631a45634fac4d96227a432
maguila-gus25 <gustavohcramos@gmail.com>
2025-08-15 00:16:03 -0300

    I did a challenge to finish the first php alura course

---
+++ desafio.php
@@ -0,0 +1,56 @@
+<?php
+
+$saldo = 1_000;
+$titularConta = "Gustavo";
+
+echo "********************\n";
+echo "Titular: $titularConta\n";
+echo "Saldo atual: $saldo\n";
+echo "********************\n";
+
+do {
+    echo "********************\n";
+    echo "Titular: $titularConta\n";
+    echo "Saldo atual: $saldo\n";
+    echo "********************\n";
+
+    echo "1. Consultar saldo atual\n";
+    echo "2. Sacar valor\n";
+    echo "3. Depositar valor\n";
+    echo "4. Sair\n";
+
+    $opcao = (int) fgets(STDIN);
+
+    switch ($opcao) {
+        case 1:
+            echo "********************\n";
+            echo "Titular: $titularConta\n";
+            echo "Saldo atual: $saldo\n";
+            echo "********************\n";
+            break;
+
+        case 2:
+            echo "Qual valor deseja sacar?\n";
+            $valorASacar = (float) fgets(STDIN);
+            if ($valorASacar > $saldo) {
+                echo "Saldo insuficiente\n";
+            } else {
+                $saldo -= $valorASacar;
+            }
+            break;
+
+        case 3:
+            echo "Quanto deseja depositar\n";
+            $valorADepositar = (float) fgets(STDIN);
+            $saldo += $valorADepositar;
+            break;
+
+        case 4:
+            echo "Até breve!\n";
+            break;
+
+        default:
+            echo "Opção Inválida\n";
+            break;
+    }
+} while ($opcao != 4);
---
+++ entrada.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Digite um número: \n";
+
+$numero = (float) fgets(STDIN);
+
+echo "O número é: $numero\n";
+
