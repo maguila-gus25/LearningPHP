a4bd67928065ed7737b5b87ca56fde66f8139233
maguila-gus25 <gustavohcramos@gmail.com>
2025-08-21 20:44:45 -0300

    __contruct()

---
+++ screen-match/src/Modelo/Conta.php
@@ -0,0 +1,47 @@
+<?php
+
+class Conta
+{
+    private int $saldoEmCentavos;
+    private string $nomeTitular;
+    private string $numeroConta;
+
+    public function depositar(int $valorADepositar): void
+    {
+        if ($valorADepositar > 0) {
+            $this->saldoEmCentavos += $valorADepositar;
+        }
+    }
+
+    public function sacar(int $valorASacar): void
+    {
+        if ($valorASacar > 0 && $valorASacar <= $this->saldoEmCentavos) {
+            $this->saldoEmCentavos -= $valorASacar;
+        }
+    }
+    
+    public function getSaldoEmCentavos(): int
+    {
+        return $this->saldoEmCentavos;
+    }
+
+    public function setNomeTitular(string $nomeTitular): void
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNumeroConta(int $numeroConta): void
+    {
+        $this->numeroConta = $numeroConta;
+    }
+
+    public function getNumeroConta(): string
+    {
+        return $this->numeroConta;
+    }
+}
--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -4,38 +4,42 @@ class Filme {
         private string $nome = 'Nome padrão';
         private int $anoLancamento = 2024;
         private string $genero = 'ação';
-        private array $notas = [];
+        private array $notas;
+
+        public function __contruct(
+            string $nome,
+            int $anoLancamento,
+            string $genero
+        ) {
+            $this->notas = [];
+        }
 
         //Getter
-        public function anoLancamento(): int {
+        public function anoLancamento(): int 
+        {
             return $this->anoLancamento;
         }
-        //Setter
-        public function defineAnoLancamento(int $anoLancamento) {
-            $this->anoLancamento = $anoLancamento;
-        }
+
         //Getter
-        public function nome(): string {
+        public function nome(): string 
+        {
             return $this->nome;
         }
-        //Setter
-        public function defineNome(string $nome) {
-            $this->nome = $nome;
-        }
+
         //Getter
-        public function genero(): string {
+        public function genero(): string 
+        {
             return $this->genero;
         }
-        //Setter
-        public function defineGenero(string $genero) {
-            return $this->genero = $genero;
-        }
 
-        public function avalia(float $nota) : void {
+
+        public function avalia(float $nota) : void 
+        {
             $this->notas[] = $nota;
         }
 
-        public function media(): float {
+        public function media(): float 
+        {
             $somaNotas = array_sum($this->notas);
             $quantidadeDeNotas = count($this->notas);
 
