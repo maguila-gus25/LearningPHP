31c0de5de9a5a1a4351253a1875aeef5236e4606
maguila-gus25 <gustavohcramos@gmail.com>
2025-08-21 19:45:05 -0300

    Encapsulating

--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -1,8 +1,44 @@
 <?php
 
 class Filme {
-        public string $nome;
-        public int $anoLancamento;
-        public string $genero;
-        public float $nota;
+        private string $nome = 'Nome padrão';
+        private int $anoLancamento = 2024;
+        private string $genero = 'ação';
+        private array $notas = [];
+
+        //Getter
+        public function anoLancamento(): int {
+            return $this->anoLancamento;
+        }
+        //Setter
+        public function defineAnoLancamento(int $anoLancamento) {
+            $this->anoLancamento = $anoLancamento;
+        }
+        //Getter
+        public function nome(): string {
+            return $this->nome;
+        }
+        //Setter
+        public function defineNome(string $nome) {
+            $this->nome = $nome;
+        }
+        //Getter
+        public function genero(): string {
+            return $this->genero;
+        }
+        //Setter
+        public function defineGenero(string $genero) {
+            return $this->genero = $genero;
+        }
+
+        public function avalia(float $nota) : void {
+            $this->notas[] = $nota;
+        }
+
+        public function media(): float {
+            $somaNotas = array_sum($this->notas);
+            $quantidadeDeNotas = count($this->notas);
+
+            return $somaNotas / $quantidadeDeNotas;
+        }
     }   
